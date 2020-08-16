92a4e29115eaf426aa5ef4cc2d80da572dfff452
Daniel Norred <Daniel@Nor.red>
2020-08-16 16:07:01 -0500

    Seperate server list into new component

---
+++ src/Components/ServerItem.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Divider, Layout, TopNavigation, Text, List, ListItem, Spinner, Card, Icon, } from '@ui-kitten/components';
+
+export default ServerItem = ({ server }) => {
+    const [serverStatus, setSetverStatus] = useState('')
+    useEffect(() => {
+        if (server.power_status == 'running') {
+            setSetverStatus('success')
+        } else {
+            setSetverStatus('danger')
+        }
+    }, [server])
+    return (
+        <Card status={serverStatus}>
+            <Layout style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                alignSelf: 'stretch',
+                textAlign: 'center',
+                justifyContent: "space-between"
+            }}>
+                <Text category={"label"}>{server.os}</Text>
+                <Button
+                    appearance='ghost'
+                    accessoryRight={props => <Icon {...props} name='power' />}
+                    status={serverStatus}
+                />
+            </Layout>
+        </Card>
+    )
+}
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button, Divider, Layout, TopNavigation, Text, List, ListItem, Spinner, Card, Icon, } from '@ui-kitten/components';
 import useApiLookup from '../Hooks/UseAPILookup'
+import ServerItem from '../Components/ServerItem'
 
 export const HomeScreen = ({ navigation }) => {
-
     const [lookupServers, serverObj, user, isLoading] = useApiLookup()
-
     if (isLoading == false) {
         return (
             <SafeAreaView style={{ flex: 1, }}>
@@ -35,33 +34,4 @@ export const HomeScreen = ({ navigation }) => {
         )
 
     }
-}
-
-const ServerItem = ({ server }) => {
-    const [serverStatus, setSetverStatus] = useState('')
-    useEffect(() => {
-        if (server.power_status == 'running') {
-            setSetverStatus('success')
-        } else {
-            setSetverStatus('danger')
-        }
-    }, server)
-    return (
-        <Card status={serverStatus}>
-            <Layout style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                alignSelf: 'stretch',
-                textAlign: 'center',
-                justifyContent: "space-between"
-            }}>
-                <Text category={"label"}>{server.os}</Text>
-                <Button
-                    appearance='ghost'
-                    accessoryRight={props => <Icon {...props} name='power' />}
-                    status={serverStatus}
-                />
-            </Layout>
-        </Card>
-    )
 }
