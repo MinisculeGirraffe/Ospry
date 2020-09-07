d371cfcc92343848307cbb5830a551763becb66b
Daniel Norred <daniel@Daniels-iMac.local.nor.red>
2020-09-07 17:55:54 -0500

    Update add server screen... again

--- src/Screens/AddServerScreen.js
+++ src/Screens/AddServerScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Layout, TopNavigation, TopNavigationAction, Icon, Divider, Text, Select, SelectGroup, SelectItem, DrawerGroup, Drawer, DrawerItem, CheckBox, Input, Button } from '@ui-kitten/components'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, ScrollView} from 'react-native'
+import { StyleSheet, ScrollView } from 'react-native'
 import Collapsible from 'react-native-collapsible';
 import useApiLookup from '../Hooks/UseAPILookup'
 
@@ -25,23 +25,15 @@ export const AddServerScreen = ({ navigation }) => {
     const [label, setLabel] = React.useState("")
 
     const [autoBackups, setAutoBackups] = React.useState(false)
-    const [IPv6, setIPv6] = React.useState(true)
+    const [IPv6, setIPv6] = React.useState('yes')
     const [privateNetworking, setPrivateNetworking] = React.useState(false)
 
-    const [serverCollapsed, setServerCollapsed] = React.useState(false)
-    const [optionsCollapsed, setOptionsCollapsed] = React.useState(true)
+    const [notCollapsedView, setNotCollapsedView] = React.useState('server')
 
+    const [apiServerInfo, setApiServerInfo] = React.useState({})
 
     const api = useApiLookup()
 
-    const styles = StyleSheet.create({
-        label: {
-
-            paddingBottom: 10,
-            paddingTop: 10
-        }
-
-    })
 
     React.useEffect(() => {
         api.lookupServerPlans()
@@ -57,7 +49,6 @@ export const AddServerScreen = ({ navigation }) => {
                 setSelectedLocation("")
             }
             setAvailServerPlans(plans)
-
         }
     }, [selectedTypeIndex])
 
@@ -102,15 +93,49 @@ export const AddServerScreen = ({ navigation }) => {
         if (selectedOSIndex) {
             let selectedFamily = Object.keys(api.serverOS)[selectedOSIndex.section]
             let osFamily = api.serverOS[selectedFamily]
-
             setSelectedOS(osFamily[selectedOSIndex.row])
         }
     }, [selectedOSIndex])
+
+    React.useEffect(() => {
+
+    }, [notCollapsedView])
+
+
+    React.useEffect(() => {
+
+    },[selectedType,selectedPlan,selectedLocation,selectedOS,IPv6,autoBackups,hostname,label])
+
+    const submitNewServer = () => {
+        //TODO Input validation
+        const serverInfo = {
+            DCID: selectedLocation.DCID,
+            VPSPLANID: selectedPlan.VPSPLANID,
+            OSID: selectedOS.OSID,
+            enable_ipv6: IPv6,
+            hostname: hostname
+        }
+        
+
+    }
+    const styles = StyleSheet.create({
+        label: {
+            paddingBottom: 10,
+            paddingTop: 10
+        }
+    })
+
 
     const goBack = () => (
         <TopNavigationAction
             icon={(props) => <Icon {...props} name='arrow-back-outline' />}
             onPress={() => navigation.goBack()}
+        />
+    )
+    const addServer = () => (
+        <TopNavigationAction
+            icon={(props) => <Icon {...props} name='plus-outline' />}
+            onPress={() => submitNewServer().then(navigation.goBack())}
         />
     )
 
@@ -123,122 +148,120 @@ export const AddServerScreen = ({ navigation }) => {
                     title={props => <Text  {...props}>New Server</Text>}
                     alignment="center"
                     subtitle={props => <Text {...props} category={'label'}>{selectedPlan !== "" ? "$" + selectedPlan.price_per_month + " / Month" : ""}</Text>}
+                    accessoryRight={addServer}
                 />
                 <Divider />
                 <ScrollView >
-                    
-               
-                <Layout >
-                    <DrawerItem
-                        title="Server Info"
-                        onPress={() => { setServerCollapsed(!serverCollapsed) }}
-                        accessoryLeft={(props) => <Icon {...props} name={'archive-outline'} />}
-                        accessoryRight={(props) => <Icon {...props} name={'arrow-down-outline'} />}
-                    />
-                    <Collapsible
-                        collapsed={serverCollapsed}
-                    >
-                        <Select
-                            label={(props) => <Text style={styles.label} category={"s1"} appearance={"hint"} > Server Type</Text>}
-                            value={selectedType}
-                            placeholder={"Select Type"}
-                            selectedIndex={selectedTypeIndex}
-                            onSelect={type => setSelectedTypeIndex(type)}
-                        >
-                            {Object.keys(api.serverAvailability.plans).map(plan => {
-                                return (
-                                    <SelectItem key={plan} title={plan}></SelectItem>
-                                )
-
-                            })}
-                        </Select>
-
-
-                        <Select
-                            label={(props) => <Text style={styles.label} category={"s1"} appearance={"hint"} > Server Plan</Text>}
-                            value={selectedPlan.name}
-                            placeholder={"Select Plan"}
-                            selectedIndex={selectedPlanIndex}
-                            onSelect={plan => setSelectedPlanIndex(plan)}
-                            disabled={availServerPlans.length == 0 ? true : false}
-                        >
-                            {availServerPlans.map(plan => {
-                                return (
-                                    <SelectItem key={plan.name} title={plan.name} />
-                                )
-                            })}
-                        </Select>
-                        <Select
-                            label={(props) => <Text style={styles.label} category={"s1"} appearance={"hint"} > Server Location</Text>}
-                            placeholder={"Select Location"}
-                            selectedIndex={selectedLocationIndex}
-                            onSelect={location => setSelectedLocationIndex(location)}
-                            disabled={availLocations.length == 0 ? true : false}
-                        >
-                            {Object.keys(availLocations).map(zone => {
-                                return (
-                                    <SelectGroup key={zone} title={zone} >
-                                        {availLocations[zone].map(location => {
-                                            return (
-                                                <SelectItem key={location.name} title={location.name} />
-                                            )
-                                        })}
-                                    </SelectGroup>
-                                )
-
-                            })}
-                        </Select>
-                        <Select
-                            label={(props) => <Text style={styles.label} category={"s1"} appearance={"hint"} > Server OS</Text>}
-                            value={selectedOS ? selectedOS.name : null}
-                            placeholder={"Select OS"}
-                            selectedIndex={selectedOSIndex}
-                            onSelect={os => setslectedOSIndex(os)}
-                        >
-                            {Object.keys(api.serverOS).map(osFamily => {
-                                return (
-                                    <SelectGroup key={osFamily} title={osFamily}>
-                                        {api.serverOS[osFamily].map(os => {
-                                            return (
-                                                <SelectItem key={os.name} title={os.name} />
-                                            )
-
-                                        })}
-                                    </SelectGroup>
-                                )
-                            })}
-                        </Select>
-                    </Collapsible>
-
-                    <DrawerItem
-                        title="Server Options"
-                        onPress={() => { setOptionsCollapsed(!optionsCollapsed) }}
-                        accessoryLeft={(props) => <Icon {...props} name={'book-open-outline'} />}
-                        accessoryRight={(props) => <Icon {...props} name={'arrow-down-outline'} />}
-                    />
-                    <Collapsible
-                        collapsed={optionsCollapsed}
-                    >
-                        <Input
-                            label={"Hostname"}
-                            value={hostname}
-                            onChangeText={nextValue => setHostname(nextValue)}
+                    <Layout >
+                        <DrawerItem
+                            title="Server Info"
+                            onPress={() => setNotCollapsedView('server')}
+                            accessoryLeft={(props) => <Icon {...props} name={'archive-outline'} />}
+                            accessoryRight={(props) => <Icon {...props} name={'arrow-down-outline'} />}
                         />
-                        <Input
-                            label={"Label"}
-                            value={label}
-                            onChangeText={nextValue => setLabel(nextValue)}
+                        <Collapsible
+                            collapsed={notCollapsedView == 'server' ? false : true}
+                        >
+                            <Select
+                                label={(props) => <Text style={styles.label} category={"s1"} appearance={"hint"} > Server Type</Text>}
+                                value={selectedType}
+                                placeholder={"Select Type"}
+                                selectedIndex={selectedTypeIndex}
+                                onSelect={type => setSelectedTypeIndex(type)}
+                            >
+                                {Object.keys(api.serverAvailability.plans).map(plan => {
+                                    return (
+                                        <SelectItem key={plan} title={plan}></SelectItem>
+                                    )
+
+                                })}
+                            </Select>
+                            <Select
+                                label={(props) => <Text style={styles.label} category={"s1"} appearance={"hint"} > Server Plan</Text>}
+                                value={selectedPlan.name}
+                                placeholder={"Select Plan"}
+                                selectedIndex={selectedPlanIndex}
+                                onSelect={plan => setSelectedPlanIndex(plan)}
+                                disabled={availServerPlans.length == 0 ? true : false}
+                            >
+                                {availServerPlans.map(plan => {
+                                    return (
+                                        <SelectItem key={plan.name} title={plan.name} />
+                                    )
+                                })}
+                            </Select>
+                            <Select
+                                label={(props) => <Text style={styles.label} category={"s1"} appearance={"hint"} > Server Location</Text>}
+                                value={selectedLocation.name}
+                                placeholder={"Select Location"}
+                                selectedIndex={selectedLocationIndex}
+                                onSelect={location => setSelectedLocationIndex(location)}
+                                disabled={availLocations.length == 0 ? true : false}
+                            >
+                                {Object.keys(availLocations).map(zone => {
+                                    return (
+                                        <SelectGroup key={zone} title={zone} >
+                                            {availLocations[zone].map(location => {
+                                                return (
+                                                    <SelectItem key={location.name} title={location.name} />
+                                                )
+                                            })}
+                                        </SelectGroup>
+                                    )
+
+                                })}
+                            </Select>
+                            <Select
+                                label={(props) => <Text style={styles.label} category={"s1"} appearance={"hint"} > Server OS</Text>}
+                                value={selectedOS ? selectedOS.name : null}
+                                placeholder={"Select OS"}
+                                selectedIndex={selectedOSIndex}
+                                onSelect={os => setslectedOSIndex(os)}
+                            >
+                                {Object.keys(api.serverOS).map(osFamily => {
+                                    return (
+                                        <SelectGroup key={osFamily} title={osFamily}>
+                                            {api.serverOS[osFamily].map(os => {
+                                                return (
+                                                    <SelectItem key={os.name} title={os.name} />
+                                                )
+
+                                            })}
+                                        </SelectGroup>
+                                    )
+                                })}
+                            </Select>
+                        </Collapsible>
+
+                        <DrawerItem
+                            title="Server Options"
+                            onPress={() => { setNotCollapsedView('options') }}
+                            accessoryLeft={(props) => <Icon {...props} name={'book-open-outline'} />}
+                            accessoryRight={(props) => <Icon {...props} name={'arrow-down-outline'} />}
                         />
-                        <DrawerItem title="IPv6" accessoryRight={() =>
-                            <CheckBox
-                                checked={IPv6}
-                                onChange={(checked) => setIPv6(!IPv6)} />
-                        } />
+                        <Collapsible
+                            collapsed={notCollapsedView == 'options' ? false : true}
+                        >
+                            <Input
+                                label={"Hostname"}
+                                value={hostname}
+                                onChangeText={nextValue => setHostname(nextValue)}
+                            />
+                            <Input
+                                label={"Label"}
+                                value={label}
+                                onChangeText={nextValue => setLabel(nextValue)}
+                            />
+                            <DrawerItem title="IPv6" accessoryRight={() =>
+                                <CheckBox
+                                    checked={IPv6 == 'yes' ? true : false}
+                                    onChange={(checked) => setIPv6(IPv6 == 'yes' ? 'no' : 'yes')} />
+                            } />
 
-                    </Collapsible>
+                        </Collapsible>
 
 
-                </Layout>
+                    </Layout>
                 </ScrollView>
             </SafeAreaView >
 
